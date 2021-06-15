5088c6df83aed149d613dd4380a540b7ee6851e1
DEPIDSVY <depidsvy@nintendo.de>
2021-06-15 13:15:35 +0200

    - load placeholder css with AJAX

---
+++ pages/api/css/[file].ts
@@ -0,0 +1,19 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import { getPlaiceholder } from "plaiceholder";
+import { FOLDER } from "../files";
+import { NextApiRequest, NextApiResponse } from "next";
+import path from "path";
+import fs from "fs";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  let imageFile = path.join(FOLDER, req.query.file as string);
+  // imageFile = imageFile.replaceAll(/\\/g, "/");
+  // imageFile = imageFile.replace(/C:/, "/c");
+  // console.log({ imageFile });
+  const imageBlob = fs.readFileSync(imageFile);
+  const { css, img } = await getPlaiceholder(imageBlob);
+  res.status(200);
+  res.setHeader("cache-control", "public, max-age=999999999");
+  res.json({ css, img });
+};
