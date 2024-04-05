6f0a649a762f3eac6420212a61f0d6b2ab3e0563
depidsvy <spidgorny@gmail.com>
2024-04-05 18:40:54 +0300

    feat: add script to handle deleted images

---
+++ lib/thumb-file.ts
@@ -0,0 +1,72 @@
+import { S3Storage } from "./S3Storage";
+import fs from "fs";
+import path from "path";
+import { S3File } from "../components/list-files";
+
+export class ThumbFile {
+  protected thumbnailFilePath: string;
+  thumbnails = [] as S3File[];
+
+  constructor(
+    protected s3: S3Storage,
+    protected prefix: string,
+  ) {
+    this.thumbnailFilePath = `${this.prefix}/.thumbnails.json`;
+  }
+
+  async init() {
+    fs.mkdirSync(path.dirname(this.thumbnailFilePath), { recursive: true });
+    let thumbnails = [];
+    if (fs.existsSync(this.thumbnailFilePath)) {
+      this.thumbnails = JSON.parse(
+        fs.readFileSync(this.thumbnailFilePath, "utf8"),
+      );
+    } else {
+      try {
+        this.thumbnails = JSON.parse(this.s3.get(this.thumbnailFilePath));
+      } catch (err) {
+        console.error(err);
+        this.thumbnails = [];
+      }
+    }
+  }
+
+  existsKey(key: string) {
+    return this.thumbnails.find((x) => x.key === key);
+  }
+
+  put(value: S3File) {
+    this.thumbnails = this.replaceOrAppend(
+      this.thumbnails,
+      value,
+      (x: S3File) => x.key === value.key,
+    );
+  }
+
+  replaceOrAppend(arr: any[], val: any, compFn: (v: any) => boolean) {
+    const res = [...arr];
+    const i = arr.findIndex(compFn);
+    // console.log("find", val.key, "=", i);
+    if (i === -1) {
+      res.push(val);
+    } else {
+      res.splice(i, 1, val);
+    }
+    return res;
+  }
+
+  async save() {
+    fs.writeFileSync(
+      this.thumbnailFilePath,
+      JSON.stringify(this.thumbnails, null, 2),
+    );
+    await this.s3.put(
+      this.thumbnailFilePath,
+      JSON.stringify(this.thumbnails, null, 2),
+    );
+  }
+
+  removeKey(key: string) {
+    this.thumbnails = this.thumbnails.filter((x) => x.key !== key);
+  }
+}
---
+++ test/run-after-delete.ts
@@ -0,0 +1,31 @@
+import { runTest } from "./bootstrap";
+import fs from "fs";
+import { getPlaiceholder } from "plaiceholder";
+import { S3File } from "../components/list-files";
+import path from "path";
+import invariant from "tiny-invariant";
+import { getS3Storage } from "../lib/S3Storage";
+import { ThumbFile } from "../lib/thumb-file";
+
+void runTest(async () => {
+  const s3 = getS3Storage();
+  const prefix = "2024 Cyprus";
+  const files: S3File[] = await s3.list({ prefix });
+  console.log("s3 files", files.length);
+  invariant(files.length, `not files in ${prefix}`);
+
+  const thumbFile = new ThumbFile(s3, prefix);
+  await thumbFile.init();
+  console.log("thumb file", thumbFile.thumbnails.length);
+  let sourceDataListIsACopy = [...thumbFile.thumbnails];
+  for (let entry of sourceDataListIsACopy) {
+    const exists = files.find((x) => x.key === entry.key);
+    console.log(exists ? "+" : "0", entry.key);
+    if (!exists) {
+      thumbFile.removeKey(entry.key);
+    }
+    delete entry.metadata.xmp;
+    thumbFile.thumbnails.push(entry);
+  }
+  await thumbFile.save();
+});
